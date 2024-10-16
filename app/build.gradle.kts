f57466298a3a1b441789be7c0973fba65fbc06fd
Bloodwingv2 <bhandariumesh81@gmail.com>
2024-10-16 21:35:24 +0530

    Updated activity_splash.xml with valid links

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,5 +60,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.dotlottie.android)
+    implementation(libs.lottie)
 }
