b263d340fc28c01e80792f8d9214ea094ce06cb0
Bloodwingv2 <bhandariumesh81@gmail.com>
2024-10-20 14:10:14 +0530

    Updated intent method for Splash screen and enabled fullscreen in java file

--- app/src/main/java/com/example/switch_it/SplashActivity.java
+++ app/src/main/java/com/example/switch_it/SplashActivity.java
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
+import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -16,6 +16,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
 
         // Initialize the Lottie animation view
