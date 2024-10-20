86dc57bb1e928a26ef8c349033b60c15576e3b9f
Bloodwingv2 <bhandariumesh81@gmail.com>
2024-10-20 16:12:35 +0530

    Adding Text to splash screen to display App name and various other animation enchancements to smoothen transition

--- app/src/main/java/com/example/switch_it/SplashActivity.java
+++ app/src/main/java/com/example/switch_it/SplashActivity.java
@@ -13,6 +13,9 @@ import com.airbnb.lottie.LottieDrawable;
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int SPLASH_DURATION = 3600; // Total duration of the splash screen in milliseconds
+    private LottieAnimationView animationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,21 +25,21 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         // Initialize the Lottie animation view
-        LottieAnimationView animationView = findViewById(R.id.splashAnimation);
+        animationView = findViewById(R.id.splashAnimation);
         animationView.setAnimation("rocketman.json"); // Ensure this matches your Lottie animation file
         animationView.setRepeatCount(LottieDrawable.INFINITE); // Loop the animation indefinitely
+        animationView.setSpeed(1); // Set the speed of the animation
         animationView.playAnimation();
-
-        // Duration for the splash screen
-        int splashDuration = 6000; // 3 seconds
 
         // Using a Handler to delay the transition to MainActivity
         new Handler().postDelayed(() -> {
+            // Stop the animation and transition to the MainActivity
+            animationView.cancelAnimation(); // Stop the animation
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
             // Apply smooth transition animation
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             finish(); // Close the SplashActivity
-        }, splashDuration);
+        }, SPLASH_DURATION);
     }
 }
